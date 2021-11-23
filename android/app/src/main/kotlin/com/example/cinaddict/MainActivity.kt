b499a4880d34bc72aaa20aff9f1815db699287a2
bilgehancoskun <bilgehan.coskun@hotmail.com>
2021-11-23 11:25:23 +0300

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/cinaddict/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cinaddict
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Welcome(),
+  ));
+}
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
