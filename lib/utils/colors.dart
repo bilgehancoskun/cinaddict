43443e78b174575b54c35e27940fe98db0893ece
ruyadinmezel <ruyabusedinmezel@gmail.com>
2021-11-28 22:04:58 +0300

    login branch complete -login view -styles -colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,1 +1,19 @@
 // TODO: Implement colors (primary color, page colors etc.)
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color red_primary = Colors.red;
+  static const Color red_secondary = const Color(0xFFCB1707);
+  static const Color red_3 = const Color(0xFFA71E10);
+  static const Color black = const Color(0xFF000000);
+  static const Color dark_grey = const Color(0xFF2B2929);
+  static const Color mid_grey = const Color(0xFF4E4C4C);
+  static const Color light_grey = const Color(0xFF6F6F6F);
+  static const Color lighter_grey = const Color(0xFFA5A4A4);
+  static const Color white = Colors.white;
+  static const Color yellow = const Color(0xFFFFCE3B);
+
+  static const Color textColor = const Color(0xFF242121);
+  static const Color headingColor = const Color(0xFFBD0202);
+  static const Color captionColor = Colors.white;
+}
--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,1 +1,23 @@
 // TODO: Implement Styles (fonts etc.)
+import 'package:cinaddict/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+
+final lightTextStyle = TextStyle(
+  color: AppColors.light_grey,
+  fontSize: 14.0,
+  letterSpacing: -0.5,
+);
+final lighterTextStyle = TextStyle(
+  color: AppColors.lighter_grey,
+  fontSize: 14.0,
+  letterSpacing: -0.7,
+);
+
+final darkTextStyle = TextStyle(
+  color: AppColors.black,
+  fontSize: 16.0,
+  letterSpacing: 0.3,
+);
+
+
