cd16d91a19ac8dd6e5c2cebf01c689ed4f8d39bc
Dogukan <digdeci@gmail.com>
2021-12-10 02:53:04 +0300

    -Added validation for signup view -Added firebase auth package -(what to do with Username text field is unknown)

--- lib/routes/signup_view.dart
+++ lib/routes/signup_view.dart
@@ -5,9 +5,13 @@ import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 class SignUpView extends StatelessWidget {
-  const SignUpView({Key? key, required this.analytics, required this.observer}) : super(key: key);
+  SignUpView({Key? key, required this.analytics, required this.observer}) : super(key: key);
   final FirebaseAnalytics analytics;
   final FirebaseAnalyticsObserver observer;
+  final _formKey = GlobalKey<FormState>();
+  String mail = "";
+  String password = "";
+  String passwordCheck="";
 
   @override
   Widget build(BuildContext context) {
@@ -16,159 +20,225 @@ class SignUpView extends StatelessWidget {
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
-                      child: Image.asset(
-                        'lib/assets/cinaddict_logo.png',
-                        width: MediaQuery.of(context).size.width / 2.2,
+            child: Form(
+              key:_formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+                        child: Image.asset(
+                          'lib/assets/cinaddict_logo.png',
+                          width: MediaQuery.of(context).size.width / 2.2,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              fillColor: AppColors.darkGrey,
-                              filled: true,
-                              hintText: 'E-mail',
-                              hintStyle: AppTextStyle.lightTextStyle,
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                fillColor: AppColors.darkGrey,
+                                filled: true,
+                                hintText: 'E-mail',
+                                hintStyle: AppTextStyle.lightTextStyle,
 
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: AppColors.midGrey,
-                                  width: 2.0,
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: AppColors.midGrey,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.all(Radius.circular(0)),
                                 ),
-                                borderRadius: BorderRadius.all(Radius.circular(0)),
                               ),
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (value){
+                                if (value == null) {
+                                  return 'E-mail field cannot be empty';
+                                } else {
+                                  String trimmedValue = value.trim();
+                                  if (trimmedValue.isEmpty) {
+                                    return 'E-mail field cannot be empty';
+                                  }
+                                  // if(!EmailValidator.validate(trimmedValue)) {//TODO ADD email_validator 2.0.1 from pub.dev
+                                  //  return 'Please enter a valid email';
+                                  // }
+                                }
+                                return null;
+                              },
+                              onSaved: (value){
+                                if (value != null) {
+                                  mail = value;
+                                }
+                              },
                             ),
-                            keyboardType: TextInputType.emailAddress,
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              fillColor: AppColors.darkGrey,
-                              filled: true,
-                              hintText: 'Username',
-                              hintStyle: AppTextStyle.lightTextStyle,
-
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: AppColors.midGrey,
-                                  width: 2.0,
-                                ),
-                                borderRadius: BorderRadius.all(Radius.circular(0)),
-                              ),
-                            ),
-                            keyboardType: TextInputType.text,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              fillColor: AppColors.darkGrey,
-                              filled: true,
-                              hintText: 'Password',
-                              hintStyle: AppTextStyle.lightTextStyle,
-
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: AppColors.midGrey,
-                                  width: 2.0,
-                                ),
-                                borderRadius: BorderRadius.all(Radius.circular(0)),
-                              ),
-                            ),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                            enableSuggestions: false,
-                            autocorrect: false,
-                          ),
-                        ),
-                        SizedBox(width: 8.0),
-                        Expanded(
-                          flex: 1,
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              fillColor: AppColors.darkGrey,
-                              filled: true,
-                              hintText: 'Password (Repeat)',
-                              hintStyle: AppTextStyle.lightTextStyle,
-
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: AppColors.midGrey,
-                                  width: 2.0,
-                                ),
-                                borderRadius: BorderRadius.all(Radius.circular(0)),
-                              ),
-                            ),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                            enableSuggestions: false,
-                            autocorrect: false,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: OutlinedButton(
-                            onPressed: () {},
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 12.0),
-                              child: Text(
-                                'Sign Up',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                            style: AppButtonStyle.primaryRedButton,
-                          ),
-                        ),
-                      ],
-                    ),
-                    TextButton(
-                      onPressed:() {
-                        Navigator.pushNamed(context, '/login');
-
-                      },
-                      child: Text(
-                        "Already have an account? Click here to login.",
-                        style: AppTextStyle.lighterTextStyle,
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                fillColor: AppColors.darkGrey,
+                                filled: true,
+                                hintText: 'Username',
+                                hintStyle: AppTextStyle.lightTextStyle,
+
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: AppColors.midGrey,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.all(Radius.circular(0)),
+                                ),
+                              ),
+                              keyboardType: TextInputType.text,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                fillColor: AppColors.darkGrey,
+                                filled: true,
+                                hintText: 'Password',
+                                hintStyle: AppTextStyle.lightTextStyle,
+
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: AppColors.midGrey,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.all(Radius.circular(0)),
+                                ),
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Password field cannot be empty';
+                                } else {
+                                  String trimmedValue = value.trim();
+                                  if (trimmedValue.isEmpty) {
+                                    return 'Password field cannot be empty';
+                                  }
+                                  if (trimmedValue.length < 8) {
+                                    return 'Password must be at least 8 characters long';
+                                  }
+                                  passwordCheck=trimmedValue;
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                if (value != null) {
+                                  password = value;
+                                }
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            flex: 1,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                fillColor: AppColors.darkGrey,
+                                filled: true,
+                                hintText: 'Password (Repeat)',
+                                hintStyle: AppTextStyle.lightTextStyle,
+
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: AppColors.midGrey,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: BorderRadius.all(Radius.circular(0)),
+                                ),
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Password field cannot be empty';
+                                } else {
+                                  String trimmedValue = value.trim();
+                                  if (trimmedValue.isEmpty) {
+                                    return 'Password field cannot be empty';
+                                  }
+                                  if (trimmedValue != passwordCheck){
+                                    return "Password is not the same";
+                                  }
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: OutlinedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  print(
+                                      'Mail: ' + mail + "\nPassword: " + password);
+                                  _formKey.currentState!.save();
+                                  print(
+                                      'Mail: ' + mail + "\nPassword: " + password);
+
+                                  //TODO  getUser();
+                                }
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                              style: AppButtonStyle.primaryRedButton,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed:() {
+                          Navigator.pushNamed(context, '/login');
+
+                        },
+                        child: Text(
+                          "Already have an account? Click here to login.",
+                          style: AppTextStyle.lighterTextStyle,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
