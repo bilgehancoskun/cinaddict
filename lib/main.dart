5367dbf4a903309420219a5d25eb7998db56bb93
bilgehancoskun <bilgehan.coskun@hotmail.com>
2021-12-12 02:55:38 +0300

    Added SHA1 and SHA256 to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cinaddict/utils/app_shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'routes/login_view.dart';
@@ -11,12 +12,15 @@ import 'routes/welcome_view.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppSharedPreferences.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(App());
 }
 
-class AppBase extends StatelessWidget {
-  const AppBase({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   static String initialRoute = decideInitialRoute();
   static FirebaseAnalytics analytics = FirebaseAnalytics();
@@ -58,40 +62,3 @@ String decideInitialRoute() {
   return isFirstLaunched ? '/walkthrough' : '/welcome';
 }
 
-class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          print(
-              "An Error Occurred While Connecting Firebase:\n ${snapshot.error.toString()}");
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          print("Successfully Connected to Firebase");
-          return AppBase();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return MaterialApp(
-          home: Center(
-            child: Text('Connecting Firebase...'),
-          ),
-        );
-      },
-    );
-  }
-}
