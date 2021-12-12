af4890f96d4da2d1b4f12fba471c9e3ebdd034ed
bilgehancoskun <bilgehan.coskun@hotmail.com>
2021-12-12 03:53:45 +0300

    Fixed bracket issue

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -22,43 +22,46 @@ class AuthService {
     }
   }
 
-  Future signupWithMailAndPass(String mail,String pass) async{
-    try{
-      UserCredential result= await _auth.createUserWithEmailAndPassword(email: mail, password: pass);
-      User user= result.user!;
-    } on FirebaseAuthException catch(e){
-      if (e.code=='weak-password'){
+  Future signupWithMailAndPass(String mail,String pass) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: mail, password: pass);
+      User user = result.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
         print('The password provided is too weak');
       }
-      else if (e.code=='email-already-in-use'){
+      else if (e.code == 'email-already-in-use') {
         print('The account already exists for that email');
       }
-    } catch(e){
-      print(e);
-   }
-   // TODO: add function sign out for the feed view
-
-  Future loginWithMailAndPass(String mail, String pass) async {
-    try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(email: mail, password: pass);
-      User user = result.user!;
-      return _userFromFirebase(user);
-    } on FirebaseAuthException catch (e) {
-      if(e.code == 'user-not-found') {
-        signupWithMailAndPass(mail, pass);
-      }
     } catch (e) {
-      print(e.toString());
-      return null;
+      print(e);
+    }
+    // TODO: add function sign out for the feed view
+
+    Future loginWithMailAndPass(String mail, String pass) async {
+      try {
+        UserCredential result = await _auth.signInWithEmailAndPassword(
+            email: mail, password: pass);
+        User user = result.user!;
+        return _userFromFirebase(user);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          signupWithMailAndPass(mail, pass);
+        }
+      } catch (e) {
+        print(e.toString());
+        return null;
+      }
+    }
+
+    Future signOut() async {
+      try {
+        return await _auth.signOut();
+      } catch (e) {
+        print(e.toString());
+        return null;
+      }
     }
   }
-
-  Future signOut() async {
-    try {
-      return await _auth.signOut();
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
-
 }
