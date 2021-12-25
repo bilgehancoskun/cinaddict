7dcaac9a444052108f46bd826f2f13f08f2129e5
Dogukan <digdeci@gmail.com>
2021-12-25 03:28:09 +0300

    -Created pictures for navigation view

--- lib/routes/navigation_view.dart
+++ lib/routes/navigation_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NavigationPage extends StatefulWidget{
@@ -13,6 +14,38 @@ class _NavigationPage extends State<NavigationPage>{
         title: Text('Navigation Page'),
         automaticallyImplyLeading: false,
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.network("https://img3.aksam.com.tr/imgsdisk/2021/11/17/t25_spider-man-no-way-home-ne-384.jpg",
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+               padding: const EdgeInsets.fromLTRB(8, 16, 8, 8),
+                  child: Expanded(
+                    child: Image.network("https://upload.wikimedia.org/wikipedia/tr/9/97/Joker_%28film%29.jpg",
+                    width: MediaQuery.of(context).size.width/3.7,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Image.network("https://i.ytimg.com/vi/2u_9nLfs8SI/maxresdefault.jpg",
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Image.network("https://i.ytimg.com/vi/ePpJDKfRAyM/movieposter.jpg"),
+                ),
+              ],
+            ),
+            Image.network("https://stayhipp.com/wp-content/uploads/2019/07/New.jpg")
+          ],
+        ),
       ),
     );
   }
